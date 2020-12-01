975644683833da30f3f1d922beb50989728e4400
mvachette <marc.vachette@gmail.com>
2020-12-01 22:36:56 +0100

    Amélioration de l'algo avec une solution vue sur reddit
    Calculer le nombre potentiel que l'on cherche, et vérifier si il est dans le tableau
    l'usage de in_array est plus performant que le parcours
    (surtout dans la solution 3)

--- src/expenseReport.php
+++ src/expenseReport.php
@@ -9,11 +9,9 @@ function computeChecksum(array $report): int
     $reportLength = count($report);
     for ($firstNumberIndex = 0; $firstNumberIndex < $reportLength-1; $firstNumberIndex++) {
         $firstNumber = $report[$firstNumberIndex];
-        for ($secondNumberIndex = $reportLength-1; $secondNumberIndex > 0; $secondNumberIndex--) {
-            $secondNumber = $report[$secondNumberIndex];
-            if ($firstNumber + $secondNumber === 2020) {
-                return $firstNumber*$secondNumber;
-            }
+        $secondNumber = 2020 - $firstNumber;
+        if (in_array($secondNumber, $report)) {
+            return $firstNumber*$secondNumber;
         }
     }
 
@@ -31,11 +29,9 @@ function computeChecksum3(array $report): int
             if ($firstNumber + $secondNumber >= 2020) {
                 continue;
             }
-            for ($thirdNumberIndex = $reportLength-1; $thirdNumberIndex > 0; $thirdNumberIndex--) {
-                $thirdNumber = $report[$thirdNumberIndex];
-                if ($firstNumber + $secondNumber + $thirdNumber === 2020) {
-                    return $firstNumber * $secondNumber * $thirdNumber;
-                }
+            $thirdNumber = 2020 - $firstNumber - $secondNumber;
+            if (in_array($thirdNumber, $report)) {
+                return $firstNumber * $secondNumber * $thirdNumber;
             }
         }
     }
