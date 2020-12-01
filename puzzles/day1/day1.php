7ba795b272d8cf9930a10a477ef996710516bdda
mvachette <marc.vachette@gmail.com>
2020-12-01 22:08:13 +0100

    Optimisation de l'algo
    En triant le tableau, on peut le parcourir dans 2 directions inverses
    (en faisant le postulat que 2 nombres au début du tableau on peut de chances
    de s'additioner pour donner la valeur cherchée (leur somme sera trop grosse)

--- puzzles/day1/day1.php
+++ puzzles/day1/day1.php
@@ -2,6 +2,13 @@
 
 require_once __DIR__ . '/../../src/expenseReport.php';
 
+$startTime = microtime(true);
 echo \MVA\AOC2020\ExpenseReport\computeChecksum(
     array_map('intval', file(__DIR__ . '/input1.txt'))
-);
+) . "\n\n";
+echo microtime(true) - $startTime;
+
+// first suboptimal solution 0.0026071071624756
+// optimized algo with sorting 0.00068783760070801
+
+
--- src/expenseReport.php
+++ src/expenseReport.php
@@ -5,8 +5,12 @@ namespace MVA\AOC2020\ExpenseReport;
 
 function computeChecksum(array $report): int
 {
-    foreach ($report as $firstNumber) {
-        foreach ($report as $secondNumber) {
+    sort($report);
+    $reportLength = count($report);
+    for ($firstNumberIndex = 0; $firstNumberIndex < $reportLength-1; $firstNumberIndex++) {
+        $firstNumber = $report[$firstNumberIndex];
+        for ($secondNumberIndex = $reportLength-1; $secondNumberIndex > 0; $secondNumberIndex--) {
+            $secondNumber = $report[$secondNumberIndex];
             if ($firstNumber + $secondNumber === 2020) {
                 return $firstNumber*$secondNumber;
             }
